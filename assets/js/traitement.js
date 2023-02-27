2c37bab2cd4774452e28de27fc030cffa3618bf1
sidspardise <sidyka01@gmail.com>
2023-02-27 11:47:19 +0100

    new commit 3

--- assets/js/traitement.js
+++ assets/js/traitement.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
          $('#cryptRow').html("");
       }
 
-      // Check if valkey not empty
+      // Check if valkey not empty test
       if (!valkey) {
          return $('#keyErrorRow').html("<span class='erreur-affiche-key'>Veuillez renseigner la clé!</span>");
       } else {
